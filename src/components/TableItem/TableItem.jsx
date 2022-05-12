bbbc8006c407ddd6b55f1d68b62a58f99cf1a277
Шепелев Евгений <shepelev.e@ad1team.ru>
2022-05-12 18:33:00 +0300

    add style

--- src/components/TableItem/TableItem.jsx
+++ src/components/TableItem/TableItem.jsx
@@ -3,7 +3,7 @@ import style from "./TableItem.module.css"
 export const TableItem = ({index, title, text}) => {
 
   return (
-    <div className={style.cell}>
+    <div className={style.row}>
      <div className={style.index}>{index}</div>
      <div className={style.title}>{title}</div>
      <div className={style.text}>{text}</div>
